614e71b4bb546105592ba87f9be9c4283c5e0189
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2016-05-14 17:46:03 +0200

    Revert "created blog service"
    This reverts commit 004d6dfcd09b15f70e1204997c25f613ee02cb2f.

--- src/services/index.js
+++ src/services/index.js
@@ -1,13 +1,11 @@
 'use strict';
-const blog = require('./blog');
 const authentication = require('./authentication');
 const user = require('./user');
 
 module.exports = function() {
   const app = this;
-
-
+  
+  
   app.configure(authentication);
   app.configure(user);
-  app.configure(blog);
 };
