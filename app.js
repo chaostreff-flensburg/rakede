cd5ce186dc945937a0bb8d7c7b53f299efb141b0
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2016-08-14 17:21:01 +0200

    basic structure & templating

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
 
+// ===============
+// Dependencies
+// ===============
+
+var express         = require("express"),
+    app             = express(),
+    server          = require('http').createServer(app),
+    bodyParser      = require('body-parser'),
+    errorHandler    = require('errorhandler'),
+    methodOverride  = require('method-override'),
+    hostname        = process.env.HOSTNAME || 'localhost',
+    PORT            = process.env.PORT || 8081,
+    publicDir       = process.argv[2] || __dirname + '/public',
+    path            = require('path'),
+    r               = require('rethinkdb'),
+    sockio          = require("socket.io"),
+    Client          = require('node-rest-client').Client,
+    client          = new Client(),
+    exphbs          = require('express-handlebars');
+
+// ====================
+// Express Config
+// ====================
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('port', PORT);
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static(publicDir));
+app.use(errorHandler({
+    dumpExceptions: true,
+    showStack: true
+}));
+
+app.start = app.listen = function(){
+    return server.listen.apply(server, arguments)
+}
+
+app.get('/', function(req, res) {
+  res.render('home', {
+    title: "hello world!"
+  })
+})
+
+app.start(PORT);
+console.log("Server showing %s listening at http://%s:%s", publicDir, hostname, PORT);
