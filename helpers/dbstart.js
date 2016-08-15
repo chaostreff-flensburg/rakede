9160911df88d0c7089d0e73fc7f64cda8ee4c260
electron-byte <nicklas.frahm@gmail.com>
2016-08-15 22:01:39 +0200

    added dbstart script

---
+++ helpers/dbstart.js
@@ -0,0 +1,48 @@
+const r = require('rethinkdb');
+
+module.exports = () => {
+
+  var connection = null;
+
+  var dbconf = {
+    host: 'localhost',
+    port: 28015
+  };
+
+  var db = {
+    name: 'rakede',
+    tables: {
+      blog: ['posts', 'categories'],
+      cms: ['sites', 'menu'],
+      events: ['participant', 'event', 'user']
+    },
+    delimiter: '_'
+  };
+
+  r.connect( dbconf, (err, conn) => {
+    if (err) console.log('ERR: No database connection!');
+
+    connection = conn;
+
+    r.dbList().contains(db.name).do(exists => {
+
+      return r.branch(exists,true,r.dbCreate(db.name));
+
+    }).run(connection, () => {
+
+      conn.use(db.name);
+
+      for (category in db.tables) {
+
+        for (table in db.tables[category]) {
+
+          r.db(db.name).tableList().contains(category + db.delimiter + db.tables[category][table]).do(exists => {
+
+            return r.branch(exists,true,r.tableCreate(category + db.delimiter + db.tables[category][table]));
+
+          }).run(connection);
+        }
+      }
+    });
+  });
+}
