d43dd08cb9ac8a35ddacf49b8abd91d16edf014e
electron-byte <nicklas.frahm@gmail.com>
2016-08-16 21:34:12 +0200

    Added config for database config and structure

--- helpers/dbstart.js
+++ helpers/dbstart.js
@@ -1,53 +1,34 @@
 const r = require('rethinkdb');
 const chron = require('async');
+const path = require('path');
+
+var dbconfig = require(path.resolve('./config/dbconfig.json'));
+var dbstructure = require(path.resolve('./config/dbstructure.json'));
 
 module.exports = () => {
 
   var connection = null;
 
-  var dbconf = {
-    host: 'localhost',
-    port: 28015
-  };
-
-  var db = {
-    name: 'rakede',
-    tables: {
-      blog: ['posts', 'categories'],
-      cms: ['sites', 'menu'],
-      events: ['events']
-    },
-    delimiter: '_'
-  };
-
-  var check = 0;
-
-  for (category in db.tables) {
-    for (table in db.tables[category]) {
-      ++check;
-    }
-  }
-
-  r.connect( dbconf, (err, conn) => {
+  r.connect( dbconfig, (err, conn) => {
     if (err) console.log('ERR: No database connection!');
 
     connection = conn;
 
-    r.dbList().contains(db.name).do(exists => {
+    r.dbList().contains(dbstructure.name).do(exists => {
 
-      return r.branch(exists,true,r.dbCreate(db.name));
+      return r.branch(exists,true,r.dbCreate(dbstructure.name));
 
     }).run(connection, () => {
 
-      connection.use(db.name);
+      connection.use(dbstructure.name);
 
-      chron.forEachOf(db.tables, (tables, category, callback) => {
+      chron.forEachOf(dbstructure.tables, (tables, category, callback) => {
 
         chron.forEachOf(tables, (table, index, callback) => {
 
-          r.db(db.name).tableList().contains(category + db.delimiter + table).do(exists => {
+          r.db(dbstructure.name).tableList().contains(category + dbstructure.delimiter + table).do(exists => {
 
-            return r.branch(exists,true,r.tableCreate(category + db.delimiter + table));
+            return r.branch(exists,true,r.tableCreate(category + dbstructure.delimiter + table));
 
           }).run(connection, () => {
               callback();
