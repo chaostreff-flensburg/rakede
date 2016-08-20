75759ed21e4152eb1272152ad2f48e21cf0b9f09
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2016-08-20 17:34:54 +0200

    removed unused stuff

--- controllers/index.js
+++ controllers/index.js
@@ -1,26 +1,13 @@
 var express = require('express');
 var router = express.Router();
-//var passport = require('passport');
 
 router.use("/blog", require("./blog"));
 router.use("/events", require("./events"));
 
 router.get('/', function(req, res) {
   res.render('home', {
-    title: "Hello World! Me no know git."
+    title: "Hello World!"
   });
 });
-
-/*
-router.get('/login',
-  passport.authorize('slack'));
-
-  router.get('/login/callback',
-    passport.authorize('slack', { failureRedirect: '/login' }),
-    function(req, res) {
-      // Successful authentication, redirect home.
-      res.redirect('/');
-});
-*/
 
 module.exports = router;
