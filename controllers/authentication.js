3527ba6f562fe600eeae2359adbfe9f308f31028
Centro1993 <jonas.leitner93@gmail.com>
2016-08-24 17:11:55 +0200

    added cms controller for sites

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -2,4 +2,8 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
+router.post('/login', function(req, res) {
+
+});
+
 module.exports = router;
