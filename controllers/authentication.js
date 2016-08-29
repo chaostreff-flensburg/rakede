49b144750bac4a9bbb5a117f2abf3c940c2a8f83
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2016-08-29 17:08:22 +0200

    only allow login from specified slack-team

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -18,14 +18,22 @@ router.get('/',
 router.get('/callback',
   passport.authenticate('slack', {team: "T03JL4VF6", failureRedirect: '/' }),
   function(req, res) {
-    // Successful authentication
-    //set session cookie
-    req.session.name = req.user.id;
-    //create user if he does not exist yet
-    users.createUser(req.user.id, req.user.username, function(err, res) {
-    });
-    //redirect to backend.
-    res.redirect('/rakede');
+
+    if (req.user.teamId != 'T03JL4VF6') {
+      req.session.destroy();
+      res.redirect('/');
+    }
+    else {
+      // Successful authentication
+      //set session cookie
+      req.session.name = req.user.id;
+      //create user if he does not exist yet
+      users.createUser(req.user.id, req.user.username, function(err, res) {
+      });
+      //redirect to backend.
+      res.redirect('/rakede');
+    }
+
   });
 
 module.exports = router;
