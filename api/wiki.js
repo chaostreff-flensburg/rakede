dd7a776453a39fd37082ae7873be0064b28afd17
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2017-02-23 01:35:59 +0100

    Added comment

--- api/wiki.js
+++ api/wiki.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 var fs = require('fs');
 var marked = require('marked');
 
-/* GET user by ID. */
+/* GET article by slug. */
 router.get('/wiki/:slug', function (req, res, next) {
   fs.readFile(__dirname + '/../content/' + req.params.slug + '.md', 'utf8', (err, data) => {
     console.log(data);
