128e6950ced7059207bd0ae1f5b329310f0ec63c
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2017-04-03 21:40:37 +0200

    Watch file changes in content-folder

--- api/wiki.js
+++ api/wiki.js
@@ -1,6 +1,10 @@
 var router = require('express').Router()
 var fs = require('fs')
+var path = require('path')
 var marked = require('marked')
+var chokidar = require('chokidar')
+
+var contentFolder = path.resolve(__dirname + '/../content/')
 
 /* GET article by slug. */
 router.get('/wiki/:slug', function (req, res, next) {
@@ -14,5 +18,18 @@ router.get('/wiki/:slug', function (req, res, next) {
     }
   });
 })
+
+
+/* file watcher */
+var watcher = chokidar.watch(contentFolder, {
+  ignored: /(^|[\/\\])\../,
+  persistent: true
+});
+
+watcher
+  .on('add', path => console.log(`File ${path} has been added`))
+  .on('change', path => console.log(`File ${path} has been changed`))
+  .on('unlink', path => console.log(`File ${path} has been removed`))
+
 
 module.exports = router
