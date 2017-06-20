5cd98386532c325ce46054868722f007dc510fe9
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2017-06-20 02:29:34 +0200

    Added simple scss grid

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,9 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: ['~assets/css/main.css'],
+  css: [
+    { src: '~assets/css/grid.scss', lang: 'scss' }
+  ],
   /*
   ** Add axios globally
   */
